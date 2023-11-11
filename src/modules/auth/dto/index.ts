8481bd737242c2526c505fe16ed09f791e1ebf52
Anne Caselato <annecaselato@gmail.com>
2023-11-11 18:11:37 -0300

    feat: add password recovery routes

--- src/modules/auth/dto/index.ts
+++ src/modules/auth/dto/index.ts
@@ -1,3 +1,5 @@
+export { RequestRecoveryInput } from './request-recovery.input';
+export { ResetPasswordInput } from './reset-password.input';
 export { SignInInput } from './sign-in.input';
 export { SignInOIDCInput } from './sign-in-oidc.input';
 export { SignInResponse } from './sign-in.response';
